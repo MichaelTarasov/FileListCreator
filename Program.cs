8654dce8461b6d53e3e5b5c20a79d0c8443ab643
Michael Tarasov <mt@hta.by>
2023-11-22 15:48:44 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string rootFolder = Path.GetDirectoryName(currentDirectory);
+
+        string[] jpgFiles = Directory.GetFiles(rootFolder, "*.jpg", SearchOption.TopDirectoryOnly);
+
+        string outputFile = Path.Combine(rootFolder, "Снято.txt");
+
+        using (StreamWriter writer = new StreamWriter(outputFile))
+        {
+            int i = 1;
+            foreach (string jpgFile in jpgFiles)
+            {
+                string fileName = Path.GetFileName(jpgFile);
+                fileName = i + ". " + fileName.Substring(0, fileName.Length - 4);
+                writer.WriteLine(fileName);
+                i++;
+            }
+        }
+
+        Console.WriteLine("Список файлов с расширением .jpg сохранен в файле Снято.txt.");
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# FileListCreator
+ Console App
